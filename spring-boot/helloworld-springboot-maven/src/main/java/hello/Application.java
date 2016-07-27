2ed0e44a0396c74d5333468dd3d847c6bcc83b10
Greg Hoelzer <ghoelzer@redhat.com>
2016-07-27 16:25:10 -0500

    Display Host/Pod Name and IP Address

--- spring-boot/helloworld-springboot-maven/src/main/java/hello/Application.java
+++ spring-boot/helloworld-springboot-maven/src/main/java/hello/Application.java
@@ -6,13 +6,24 @@ import org.springframework.boot.bind.RelaxedPropertyResolver;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @SpringBootApplication
 @RestController
 public class Application {
 
 	@RequestMapping("/")
 	public String home() {
-		return "Hello Docker World";
+	InetAddress ip;
+        String hostname;
+        try {
+            ip = InetAddress.getLocalHost();
+            hostname = ip.getHostName();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+		return "Hello Docker World from " + hostname + "IP Address: " + ip;
 	}
 
 
